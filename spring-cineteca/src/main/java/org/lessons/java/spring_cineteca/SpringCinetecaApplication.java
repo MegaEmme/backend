2f4b93fd387c08d702414ffaaa2a3874a2de653c
MegaEmme <mikmusco@gmail.com>
2025-09-11 12:16:15 +0200

    feat: scaffolding

---
+++ spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/SpringCinetecaApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_cineteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCinetecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCinetecaApplication.class, args);
+	}
+
+}
---
+++ spring-cineteca/src/test/java/org/lessons/java/spring_cineteca/SpringCinetecaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.spring_cineteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCinetecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

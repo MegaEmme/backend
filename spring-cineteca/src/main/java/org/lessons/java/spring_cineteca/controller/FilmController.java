509ae2fe2b1065e82d8c13e668accce73e0bea26
MegaEmme <mikmusco@gmail.com>
2025-09-11 15:56:44 +0200

    feat: create query custom

--- spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/controller/FilmController.java
+++ spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/controller/FilmController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/films")
@@ -17,11 +19,24 @@ public class FilmController {
     @Autowired
     private FilmRepository filmRepository;
 
+    // INDEX
     @GetMapping
-    public String index(Model model) {
-
-        List<Film> films = filmRepository.findAll();
+    public String index(@RequestParam(name = "searchTerm", required = false) String searchTerm, Model model) {
+        List<Film> films;
+        if (searchTerm != null && !searchTerm.isBlank()) {
+            films = filmRepository.findByTitleContaining(searchTerm);
+        } else {
+            films = filmRepository.findAll();
+        }
         model.addAttribute("films", films);
         return "films/index";
+    }
+
+    // SHOW
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        Film film = filmRepository.findById(id).get();
+        model.addAttribute("film", film);
+        return "films/show";
     }
 }
--- spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/repository/FilmRepository.java
+++ spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/repository/FilmRepository.java
@@ -1,8 +1,10 @@
 package org.lessons.java.spring_cineteca.repository;
 
+import java.util.List;
+
 import org.lessons.java.spring_cineteca.model.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FilmRepository extends JpaRepository<Film, Integer> {
-
+    public List<Film> findByTitleContaining(String title);
 }

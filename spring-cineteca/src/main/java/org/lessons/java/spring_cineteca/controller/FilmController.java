7cf288253cebe18530ee69559b6057dd83d86fe0
MegaEmme <mikmusco@gmail.com>
2025-09-11 15:13:11 +0200

    feat: linked FilmController to repository and create index()

--- spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/controller/FilmController.java
+++ spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/controller/FilmController.java
@@ -1,5 +1,10 @@
 package org.lessons.java.spring_cineteca.controller;
 
+import java.util.List;
+
+import org.lessons.java.spring_cineteca.model.Film;
+import org.lessons.java.spring_cineteca.repository.FilmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/films")
 public class FilmController {
 
+    @Autowired
+    private FilmRepository filmRepository;
+
     @GetMapping
     public String index(Model model) {
+
+        List<Film> films = filmRepository.findAll();
+        model.addAttribute("films", films);
         return "films/index";
     }
 }

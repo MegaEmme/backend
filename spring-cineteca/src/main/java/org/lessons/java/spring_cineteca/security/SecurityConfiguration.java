2a25bdd49a2d85ddd10fc44d970daeca0f0f0d9a
MegaEmme <mikmusco@gmail.com>
2025-09-14 23:44:41 +0200

    feat: add SecurityFilterChain and PasswordEncoder beans

---
+++ spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/security/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package org.lessons.java.spring_cineteca.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                // Rotta base (localhos:8080)
+                .requestMatchers("/").hasAnyAuthority("USER", "ADMIN")
+                // I percorsi per creare e modificare i film e le categorie sono per soli
+                // ADMIN.
+                .requestMatchers("films/create", "films/edit/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/films/**").hasAuthority("ADMIN")
+                .requestMatchers("/categories", "/categories/**").hasAuthority("ADMIN")
+                // I percorsi per i film sono accessibili sia da USER che da ADMIN.
+                .requestMatchers("/films", "/films/**").hasAnyAuthority("USER", "ADMIN")
+                // Tutti gli altri percorsi sono accessibili a chiunque.
+                .requestMatchers("/**").permitAll());
+
+        http.formLogin(Customizer.withDefaults());
+        http.logout(Customizer.withDefaults());
+        http.exceptionHandling(Customizer.withDefaults());
+
+        // Disabilito CORS e CSRF, che non si trovano nel blocco authorizeHttpRequests
+        http.cors(cors -> cors.disable());
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+}

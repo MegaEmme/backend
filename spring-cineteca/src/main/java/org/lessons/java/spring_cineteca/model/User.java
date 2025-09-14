f83c79542e0dc245e4c44e04b0c388b3d32f2bcd
MegaEmme <mikmusco@gmail.com>
2025-09-14 23:24:44 +0200

    feat: create User and Role entities

---
+++ spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/model/User.java
@@ -0,0 +1,50 @@
+package org.lessons.java.spring_cineteca.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Username cannot be blank, empty, or null")
+    private String username;
+
+    @Size(min = 6, message = "Passworb must have at least 6 characters")
+    @NotBlank(message = "Password cannot be blank, empty, or null")
+    private String password;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}

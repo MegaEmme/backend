761584d65be4c4b5a0a68129252a8884b4f74856
MegaEmme <mikmusco@gmail.com>
2025-09-14 17:34:53 +0200

    feat: create filmRestController

---
+++ spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/controller/FilmRestController.java
@@ -0,0 +1,10 @@
+package org.lessons.java.spring_cineteca.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/films")
+public class FilmRestController {
+
+}

0149423588dcd80bbde2a8f539bc2b5e103eae99
MegaEmme <mikmusco@gmail.com>
2025-09-14 13:13:45 +0200

    feat:add  create and update logics in FilmController

--- spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/controller/FilmController.java
+++ spring-cineteca/src/main/java/org/lessons/java/spring_cineteca/controller/FilmController.java
@@ -7,9 +7,14 @@ import org.lessons.java.spring_cineteca.service.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/films")
@@ -50,4 +55,39 @@ public class FilmController {
         model.addAttribute("film", film);
         return "films/show";
     }
+
+    // CREATE
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("film", new Film());
+        return "films/create-or-edit";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("film") Film formFilm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "films/create-or-edit";
+        }
+        filmService.create(formFilm);
+        return "redirect:/films";
+    }
+
+    // UPDATE
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("film", filmService.getById(id));
+        model.addAttribute("edit", true);
+        return "films/create-or-edit";
+    }
+
+    @PostMapping("edit/{id}")
+    public String update(@Valid @ModelAttribute("film") Film formFilm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "films/create-or-edit";
+        }
+        filmService.update(formFilm);
+        return "redirect: /films";
+    }
+
+    // DELETE
 }
